5580c66988078b1c3a6333f0038acdebfc0ba590
yanhengfu <yanhengfu@yhf.local>
2021-12-27 15:14:29 +0800

    aaa

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    //检测到的每个组件页面，会生成导航链接
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
+}
